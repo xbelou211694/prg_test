b1ba5d94cd02c54ac1c9aba2556db47568870038
Anastasia Belousova <nastia.belousova2000@gmail.com>
2022-03-29 09:39:31 +0200

    Add cykle

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,8 +1,13 @@
 def is_natural_number(number):
-    if number % 1 == 0 and number > 0:
-        return True
-    else:
-        return False
+    my_numbers = [0, 2, 8, 7.4]
+    is_natural = []
+    for i in range(len(my_numbers)):
+        if i % 1 == 0 and i > 0:
+            is_natural.append(True)
+        else:
+            is_natural.append(False)
+
+    return is_natural
 
 def is_even_number(number):
     if number % 2 == 0:
@@ -15,7 +20,8 @@ def is_even_number(number):
 
 def main():
     my_number = 5
-    print(is_natural_number(my_number))
+    my_numbers = [0, 2, 8, 7.4]
+    print(is_natural_number(my_numbers))
     print(is_even_number(my_number))
 
 if __name__ == "__main__":
